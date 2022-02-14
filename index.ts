81535a205dd0f4bbb7b5a5b860f73944518fc11f
gurujowa <gurujowa@gmail.com>
2022-02-14 00:49:03 +0000

    up to 500 each system

--- index.ts
+++ index.ts
@@ -4,10 +4,46 @@ import moralis from "moralis/node";
 import fs from 'fs/promises';
 import { stringify } from 'csv-stringify/sync';
 import nftconfig from "./nftconfig.json";
+import internal from 'stream';
 dotenv.config();
 
 
+type OwnerResult = {
+  token_address: string;
+  token_id: string;
+  contract_type: string;
+  owner_of: string;
+  block_number: string;
+  block_number_minted: string;
+  token_uri?: string | undefined;
+  metadata?: string | undefined;
+  synced_at?: string | undefined;
+  amount?: string | undefined;
+  name: string;
+  symbol: string;
+}[];
+
 getOwners();
+
+async function MoralisEach(total: number) {
+  let eachResult: OwnerResult = [];
+  let options;
+  let offset = 0;
+
+  console.log("First each process.");
+  for (let offset = 0; offset < total; offset += 500) {
+    options = { address: nftconfig.contract_address, offset: offset };
+    console.log("Each process running. offset=" + offset);
+    let data = await moralis.Web3API.token.getNFTOwners(options);
+    if (!data.result) {
+      console.log("data result is empty. Please check your script");
+      return eachResult;
+    }
+    let res: OwnerResult = data.result;
+    eachResult = eachResult.concat(res);
+  }
+  return eachResult;
+}
 
 async function getOwners() {
   try {
@@ -17,13 +53,20 @@ async function getOwners() {
     const data = await moralis.Web3API.token.getNFTOwners(options);
 
 
-    const json = JSON.stringify(data, null, 2);
-    await fs.writeFile(nftconfig.output_json_file_name, json);
+    await fs.writeFile("./output/first_send.json", JSON.stringify(data, null, 2));
     if (!data.result) {
       Error("data result is null");
       return;
     }
-    const csv = stringify(data.result, { header: true });
+
+    let result: OwnerResult = data.result;
+
+    if (data.total && data.total >= 500) {
+      result = await MoralisEach(data.total);
+    }
+
+    await fs.writeFile(nftconfig.output_json_file_name, JSON.stringify(result, null, 2));
+    const csv = stringify(result, { header: true });
     await fs.writeFile(nftconfig.output_csv_file_name, csv);
     console.log("Data write finished");
 
